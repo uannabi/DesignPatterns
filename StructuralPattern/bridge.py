a0b5ed08d9566e84015a29de9fe2ff1a8644cfc1
uannabi <zahid_sc@hotmail.fr>
2020-09-09 13:41:33 +0600

    bridge by example

---
+++ StructuralPattern/bridge.py
@@ -0,0 +1,43 @@
+class DrawingAPIOne(object):
+    """Implementation-specific abstraction: concrete class one"""
+    def draw_circle(self,x,y,radius):
+        print("Api 1 drawing a circle at ({}, {} with radisu {}!)".format(x,y, radius))
+
+class DrawingAPITwo(object):
+    """Implementation-specific abstraction: concrete class two"""
+    def draw_circle(self,x,y,radius):
+        print("Api 1 drawing a circle at ({}, {} with radisu {}!)".format(x, y, radius))
+
+class Circle(object):
+    """Implementation-independent abstraction: for example , there could be rectangle class"""
+    def __init__(self,x,y,radius, drawing_api):
+        """Initialize the necessary attribute"""
+        self._x = x
+        self._y = y
+        self._radius = radius
+        self._drawing_api = drawing_api
+
+    def draw(self):
+        """Implementation-specific abstraction taken care of by another class DrawingAPI"""
+        self._drawing_api.draw_circle(self._x, self._y, self._radius)
+        # self._drawing_api.draw_circle(self._x, self._y, self._radius)
+
+    def scale(self, percent):
+        """Implementation-Independent"""
+        self._radius *= percent
+
+
+
+#build the first cirlce object using API one
+circel = Circle(1,2,3, DrawingAPIOne())
+
+#draw a circle
+circel.draw()
+
+#build the second circle object using two
+circel1 = Circle(2,3,4, DrawingAPITwo())
+
+#draw a circle
+circel1.draw()
+
+
